5e6325806cd09cf84d9712c3fa4569a15600e386
lilingyu0311 <38451493+lilingyu0311@users.noreply.github.com>
2018-04-24 14:57:07 +0900

    Update main.js

--- W05/main.js
+++ W05/main.js
@@ -77,7 +77,7 @@ function main()
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-    document.addEventListener( 'mousedown', mouse_down_event );
+    /*document.addEventListener( 'mousedown', mouse_down_event );
     function mouse_down_event( event )
     {
         // Clicked point in window coordinates.
@@ -109,7 +109,7 @@ function main()
             intersects[0].face.color.setRGB( 1, 0, 0 );
             intersects[0].object.geometry.colorsNeedUpdate = true;
         }
-    }
+    }*/
 
     loop();
 
