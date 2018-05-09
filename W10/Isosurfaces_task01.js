78f32e552988a9e9e91a91ca7ba2835a8728e48f
lilingyu0311 <38451493+lilingyu0311@users.noreply.github.com>
2018-05-09 14:07:49 +0900

    Update Isosurfaces_task01.js

--- W10/Isosurfaces_task01.js
+++ W10/Isosurfaces_task01.js
@@ -62,7 +62,21 @@ function Isosurfaces( volume, isovalue )
 
     geometry.computeVertexNormals();
 
-    material.color = new THREE.Color( "white" );
+    // *****Task01 Modify Start*****
+    // Create a color map
+    var cmap = [];
+    for ( var i = 0; i < 256; i++ )
+    {
+        var S = i / 255.0; // [0,1]
+        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
+        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
+        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
+        var color = new THREE.Color( R, G, B );
+        cmap.push( [ S, '0x' + color.getHexString() ] );
+    }
+    
+    material.color = new THREE.Color().setHex(cmap[isovalue][1]);
+    // *****Task01 Modify End*****
 
     return new THREE.Mesh( geometry, material );
 
